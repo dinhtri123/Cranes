b1c28ca44199f608825df792a357fdfb7768c9d3
dinhtri123 <phamdinhtriqs111@gmail.com>
2025-04-09 00:40:29 +0700

    page news

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ $(document).ready(function () {
     $(".btn-menu").on('click', function( ) {
       $('.header-menu').toggleClass('active')
     })
-  }
+  };
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tabBtn = document.querySelectorAll('.news-all-tab button')
+  const tabContent = document.querySelectorAll('.news-all-item')
+  tabBtn.forEach(btn => {
+    btn.addEventListener('click', function() {
+      tabBtn.forEach(el => el.classList.remove('active'));
+      btn.classList.add('active')
+      const dataId= btn.getAttribute('data-id');
+
+      tabContent.forEach(el => el.classList.remove('active'))
+      const dataContent = Array.from(tabContent).find(el => el.getAttribute('data-content') === dataId);
+      dataContent.classList.add('active')
+
+    })
+  })
 })
